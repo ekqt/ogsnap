2d762c9977a5e29e81786799ffba2fbf19bc7807
Hector Sosa <hello@hectorsosa.me>
2023-05-01 22:40:00 +0200

    Adjusting logo

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -2,7 +2,7 @@ export const Logo = () => {
   return (
     <div className="flex items-center gap-1">
       <span className="font-extrabold tracking-tight text-3xl">OpenGraph</span>
-      <div className="border-[2.5px] rounded  border-slate-900 h-6 w-6 flex items-center justify-center">
+      <div className="border-[2.5px] rounded  border-slate-900 dark:border-slate-50 h-6 w-6 flex items-center justify-center">
         <span className="font-extrabold tracking-tight text-base">ui</span>
       </div>
     </div>
